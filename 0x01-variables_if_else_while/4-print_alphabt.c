b548c585d2f03a28894e067f5f1643191e7199f5
Mesekir19 <88715684+Mesekir19@users.noreply.github.com>
2022-03-11 21:34:48 +0300

    Update 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,18 +1,21 @@
 #include <stdio.h>
 
 /**
- *main - print 0123456789
+ *main - print lowercase alpha a-z but remove 'q' and 'e'
  *Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	int n = 0;
+	char alpha = 'a';
 
-	while (n < 10)
+	while (alpha <= 'z')
 	{
-		printf("%i", n);
-		n++;
+		if ((alpha != 'q') && (alpha != 'e'))
+		{
+			putchar(alpha);
+		}
+		alpha++;
 	}
 	putchar('\n');
 
