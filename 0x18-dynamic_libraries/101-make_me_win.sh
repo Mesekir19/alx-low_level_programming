567e9f655fbd697b1552bfe87dc1f8d7828527e4
Mesekir19 <mesekirsalgech@example.com>
2022-06-14 00:01:39 +0300

    commit for dynamic lib

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -O /tmp/gm.so https://github.com/Mesekir19/holbertonschool-low_level_programming/master/0x18-dynamic_libraries/win.so
+wget -P /tmp/gm.so https://github.com/Mesekir19/holbertonschool-low_level_programming/master/0x18-dynamic_libraries/gm.so
 export LD_PRELOAD=/tmp/gm.so
